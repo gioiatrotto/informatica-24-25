4f7f8949018c986c6895c19d7d786d3b9411cca2
gioiatrotto <gioia.trotto@galileiostiglia.edu.it>
2025-01-09 10:54:12 +0000

    g

---
+++ informatica/literazioni/09012025.c
@@ -0,0 +1,19 @@
+/* scrivi un programma che, dati in  input n numeri interi
+e un numero x determini:
+- quanti numeri sono maggiori di x
+-quanti numeri sono minori di x
+-quanti numeri sono uguali a x*/
+#include <stdio.h>
+int main(){
+    int n=0,x=0, cnt=0;
+    printf("inserisci il valore che vuoi controllare: ");
+    scanf("%d", &x);
+    for(int i=0; i<10; i++){
+        printf("inserisci un numero: ");
+        scanf("%d", &n);
+        if(n>x){
+            cnt++
+        }
+    }
+    printf("i numeri maggiori di %d sono: %d", x,cnt);
+}
---
+++ informatica/literazioni/numeroperfetto_09012025.c
@@ -0,0 +1,12 @@
+/* es pag 223 n.17
+    Scrivi un programma che ricerca i primi tre numeri perfetti
+    e li visualizza sullo schermo
+    es: n=6 divisori=1,2,3 somma=1+2+3=6
+    6-28-496
+*/
+#include <stdio.h>
+int main(){
+    int cnt=0;
+
+    
+}
