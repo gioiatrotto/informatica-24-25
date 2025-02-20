585f64e220f5b591cb1edb30f8c4802043de0122
gioiatrotto <gioia.trotto@galileiostiglia.edu.it>
2025-02-20 10:32:26 +0000

    msg

--- informatica/funzioni/fattori.c
+++ informatica/funzioni/fattori.c
@@ -1,10 +1,24 @@
 /*dato un valore stampare i suoi fattori
 es: 28=2*2*7   2,2,7 */
 #include <stdio.h>
+void fattori(int *_n);
 int main(){
-    int n;
+    int n=0;
     do{
     printf("inserisci un valore: ");
     scanf("%d", &n); 
-    }while(n<=0)
+    }while(n<=0);
+    fattori(&n);
+
+}
+void fattori(int *_n){
+    int i=2;
+    while(*_n!=1){
+        if(*_n%i==0){
+            *_n/=i;
+            printf("%d*",i);
+        }
+        else
+            i++;
+    }
 }
