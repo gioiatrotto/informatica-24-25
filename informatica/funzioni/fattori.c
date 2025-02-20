3bcff2d58cbfc5d77c09e263e845c520f6b5a3fe
gioiatrotto <gioia.trotto@galileiostiglia.edu.it>
2025-02-20 09:52:29 +0000

    d

---
+++ informatica/funzioni/fattori.c
@@ -0,0 +1,10 @@
+/*dato un valore stampare i suoi fattori
+es: 28=2*2*7   2,2,7 */
+#include <stdio.h>
+int main(){
+    int n;
+    do{
+    printf("inserisci un valore: ");
+    scanf("%d", &n); 
+    }while(n<=0)
+}
