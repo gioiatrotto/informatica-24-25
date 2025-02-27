4295243d46b205dbc41533dbc368aed55025598e
gioiatrotto <gioia.trotto@galileiostiglia.edu.it>
2025-02-27 09:49:08 +0000

    msg

--- informatica/funzioni/Floyd.c
+++ informatica/funzioni/Floyd.c
@@ -6,6 +6,23 @@ n=5
 7 8 9 10
 11 12 13 14 15*/
 #include <stdio.h>
+void TriangoloFloyd(int *_n);
 int main(){
+    int n;
+    do{
+        printf("inserisci un valore: ");
+        scanf("%d", &n);
+    }while(n<=0);
 
+    TriangoloFloyd(&n);
+}
+void TriangoloFloyd(int *_n){
+    int cnt=0;
+    for(int i=1; i<=*_n; i++){
+        for(int j=1; j<=i; j++){
+            cnt++;
+            printf("%d\t", cnt);
+        }
+        printf("\n");
+    }
 }
