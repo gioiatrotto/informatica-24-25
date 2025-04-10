9b76b719439809fd607e0b99ec386dd4a9b96953
gioiatrotto <gioia.trotto@galileiostiglia.edu.it>
2025-04-10 08:35:03 +0000

    f

---
+++ informatica/array/es1_10042025.c
@@ -0,0 +1,12 @@
+/*Caricare
+da tastiera un vettore di N elementi. Scorrendo il vettore, al primo
+zero incontrato stampare la somma di tutti gli elementi considerati e
+quanti ne hai sommati*/
+#include <stdio.h>
+#include "libreria.c"
+#define DIM 6
+int main(){
+    int vettore[DIM]={0};
+    richiediValori(vettore,DIM);
+
+}
