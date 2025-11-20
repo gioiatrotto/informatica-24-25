e8ff3229030d1015110edcc50b1227e1683f9ba4
gioiatrotto <gioia.trotto@galileiostiglia.edu.it>
2025-11-20 07:43:36 +0000

    0è

--- info4i/info4I/stringhe/sottostringa.c
+++ info4i/info4I/stringhe/sottostringa.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef char* Stringa;
-int sottostringa(Stringa s1, Stringa s2){
-    int dim1 = strlen(s1), dim2= strlen(s2), flag=0;
-    for(int i = 0; i <=dim1-dim2+1; i++){
-        if(s1[i] == s2[0]){
-            flag = 1;
-            for(int j = 1; j < dim2; j++){
-                if(s1[i+j] != s2[j]){
+int stringa_in_stringa(const char *s1, const char *s2) {
+    int dim1 = strlen(s1);
+    int dim2 = strlen(s2);
+    int cnt = 0;
+
+    if (dim2 == 0) return 0;
+
+    for (int i = 0; i <= dim1 - dim2; i++) {
+        int flag = 1;
+        for (int j = 0; j < dim2; j++) {
+            if (s1[i + j] != s2[j]) {
                 flag = 0;
                 break;
-                }
-            }
-            if(flag == 1){
-                return i;
             }
         }
+        cnt += flag;
     }
-    return -1; 
+    return cnt;
 }
-int main(){
+int main() {
     char s1[100];
     char s2[100];
-    int posizione_sottostringa = 0;
-    printf("inserisci una srtringa: ");
+    printf("Inserisci una stringa: ");
     fgets(s1, 100, stdin);
-    printf("inserisci una srtringa: ");
-    fgets(s2, 100, stdin );
-    posizione_sottostringa = sottostringa(s1, s2);
-    if(posizione_sottostringa != -1){
-        printf("sottostringa trovata alla posizione %d\n", posizione_sottostringa);
-    } else {
-        printf("sottostringa non trovata\n");
-    }
+    s1[strcspn(s1, "\n")] = 0;//tolgo il \n
+    printf("Inserisci una sottostringa: ");
+    fgets(s2, 100, stdin);
+    s2[strcspn(s2, "\n")] = 0;//tolgo il \n
+    int occorrenze = stringa_in_stringa(s1, s2);
+    printf("la parola compare %d volte ", occorrenze);
+
+    return 0;
 }
